6d80b554093533af4bd25c48bbdab32eee9bbe50
do-not-do-that <rl7968@naver.com>
2021-06-22 03:31:33 +0900

    [알고리즘]뒤집은 소수

--- AA.py
+++ AA.py
@@ -1,16 +1,24 @@
 import sys
-# sys.stdin = open('./input.txt','rt')
+sys.stdin = open('./input.txt','rt')
 
-n = int(input())
-ch = [0]*(n+1)
-cnt = 0
+n = int(input())    
+a = list(input().split())
 
-for i in range(2,n+1):
-    if ch[i]==0:
-        cnt+=1
-        for j in range(i,n+1,i):
-            ch[j]=1
+def reverse(x):
+    return int(x[::-1])
 
-print(cnt)        
+def isPrime(x):
+    if x == 1:
+        return False
+    for i in range(2,x//2+1):
+        if x % i == 0:
+            return False
+    
+    return True
 
+for idx, x in enumerate(a):
+    a[idx] = reverse(x)
+    if isPrime(a[idx]) == True:
+        print(a[idx],end=' ')
 
+    
