79ef8a601a56407c06799cee50e9d7af7bda59f7
do-not-do-that <rl7968@naver.com>
2021-06-21 23:53:49 +0900

    [알고리즘]K번째 수

--- AA.py
+++ AA.py
@@ -1,18 +1,12 @@
 import sys
 # sys.stdin = open('./input.txt','rt')
 
-n, k = map(int,input().split())
-cnt = 0
+t = int(input())
 
-for i in range(1,n+1):
-    if n % i == 0:
-        cnt += 1
-    if cnt == k :
-        print(i)
-        break
-else:
-    print(-1)
-
-
-    
+for i in range(t):
+    n,s,e,k = map(int,input().split())
+    li = list(map(int,input().split()))
+    tmp = li[s-1:e]
+    tmp.sort()
+    print('#%d %d'%(i+1,tmp[k-1]))
 
