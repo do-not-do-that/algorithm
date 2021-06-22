498818bff46f10a2868bbf388d6057186f71deab
do-not-do-that <rl7968@naver.com>
2021-06-22 14:34:31 +0900

    [알고리즘]곳감

--- AA.py
+++ AA.py
@@ -1,41 +1,36 @@
 import sys
-sys.stdin =open('./input.txt','rt')
+# sys.stdin =open('./input.txt','rt')
 
 n = int(input())
 a = [list(map(int,input().split())) for _ in range(n)]
 
-s=e=n//2
-res=0
-for i in range(n):
-    for j in range(s,e+1):
-            res+=a[i][j]
-    if i < n//2:
-        s-=1
-        e+=1
+m = int(input())
+cmd = [list(map(int,input().split())) for _ in range(m)]
+s = 0
+e = n
+tot =0
+for i in range(m):
+    #왼쪽이라면
+    if cmd[i][1]==0:
+        for j in range(cmd[i][2]):
+            tmp1 = a[cmd[i][0]-1]
+            a[cmd[i][0]-1].append(tmp1.pop(0))
     else:
+        for j in range(cmd[i][2]):
+            tmp1 = a[cmd[i][0]-1]
+            a[cmd[i][0]-1].insert(0,tmp1.pop())
+
+for i in range(n):
+    for j in range(s,e):
+        tot += a[i][j]
+    if i < n//2:
         s+=1
         e-=1
+    else:
+        s-=1
+        e+=1
+print(tot)
 
-print(res)
-        
-    
-
-
-# 내 풀이
-
-# mt = n//2
-# pointer = 0
-# tot = 0
-
-# for i in range(n):
-#     if i <= mt:
-#         for j in range(mt-i,mt+i+1):
-#             tot+=a[i][j]
-#     else:
-#         for j in range(mt-(n-i-1),mt+(n-i)):
-#             tot+=a[i][j]
-
-# print(tot)
 
 
 
