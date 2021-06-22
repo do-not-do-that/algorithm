abd803211583ce15f696cbd1004a5e41b11f311a
do-not-do-that <rl7968@naver.com>
2021-06-22 13:49:42 +0900

    [알고리즘]사과나무

--- AA.py
+++ AA.py
@@ -1,32 +1,41 @@
 import sys
-# sys.stdin = open('./input.txt','rt')
+sys.stdin =open('./input.txt','rt')
 
 n = int(input())
-a=[list(map(int,input().split())) for _ in range(n)]
+a = [list(map(int,input().split())) for _ in range(n)]
 
-largest = -2147000000
-
+s=e=n//2
+res=0
 for i in range(n):
-    sum1 = sum2 =0
-    for j in range(n):
-        sum1+=a[i][j]
-        sum2+=a[j][i]
-    if sum1 > largest:
-        largest= sum1
-    if sum2 > largest:
-        largest= sum2
+    for j in range(s,e+1):
+            res+=a[i][j]
+    if i < n//2:
+        s-=1
+        e+=1
+    else:
+        s+=1
+        e-=1
 
-sum1=sum2=0
+print(res)
+        
+    
 
-for i in range(n):
-    sum1+=a[i][i]
-    sum2+=a[i][n-1-i]
-    if sum1 > largest:
-        largest= sum1
-    if sum2 > largest:
-        largest= sum2
 
-print(largest)
+# 내 풀이
+
+# mt = n//2
+# pointer = 0
+# tot = 0
+
+# for i in range(n):
+#     if i <= mt:
+#         for j in range(mt-i,mt+i+1):
+#             tot+=a[i][j]
+#     else:
+#         for j in range(mt-(n-i-1),mt+(n-i)):
+#             tot+=a[i][j]
+
+# print(tot)
 
 
 
