c5769a2e6d4b3eaa8a647905b28c1ba6000aa548
do-not-do-that <rl7968@naver.com>
2023-08-22 23:48:39 +0900

    feat : 88 번 풀이

---
+++ leetcode/88.py
@@ -0,0 +1,39 @@
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+# The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+# To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+# and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+
+# 풀이 1
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        tmp = nums1[:m] + nums2
+        tmp.sort()
+
+        for idx in range(m + n):
+            print(idx)
+            nums1[idx] = tmp[idx]
+
+
+# 풀이 2
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        del nums1[m:]
+        nums1 += nums2
+        nums1.sort()
+
+
+# O(M+N) time, O(1) space
