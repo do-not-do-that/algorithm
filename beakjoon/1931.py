28c97f2762eb449a9387cb1c9275559f4299f605
do-not-do-that <rl7968@naver.com>
2024-01-19 07:12:57 +0900

    백준 1931 재풀이

--- beakjoon/1931.py
+++ beakjoon/1931.py
@@ -1,15 +1,19 @@
 # 풀었던 문제.
-n = int(input())
-data = [list(map(int, input().split())) for _ in range(n)]
-data.sort(key=lambda x: (x[1], x[0]))
+# 회의실을 최대로 사용하기 - 끝나는 시간이 짧은 것들만 선택 -> 그리디
+import sys
 
-endpoint = 0  # 끝나는 시간
+input = sys.stdin.readline
+
+N = int(input())
+info = [list(map(int, input().split())) for _ in range(N)]
+info.sort(key=lambda x: (x[1], x[0]))
+
+end_time = 0
 cnt = 0
 
-for start, end in data:
-    if start >= endpoint:
-        endpoint = end
+for start, end in info:
+    if start >= end_time:
+        end_time = end
         cnt += 1
-
 
 print(cnt)
