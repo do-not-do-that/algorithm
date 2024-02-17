f505290dc6e8c273efc708af57953608f807b365
do-not-do-that <rl7968@naver.com>
2024-02-17 11:12:00 +0900

    백준 18352 풀이

--- beakjoon/18352.py
+++ beakjoon/18352.py
@@ -1,23 +1,35 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 
+# 도시 개수, 도로 개수, 거리 정보, 출발도시 번호
 N, M, K, X = map(int, input().split())
 load = {key: [] for key in range(1, N + 1)}
 
-# 길 생성하기
+# 길 생성하기 (갈 수 있는 것만 체크해야함.)
 for _ in range(M):
     A, B = map(int, input().split())
-    if B not in load[A]:
-        load[A].append(B)
-    if A not in load[B]:
-        load[B].append(A)
+    load[A].append(B)
 
-# BFS
+# load  =  {1: [2, 3], 2: [3, 4], 3: [], 4: []}
 
-# K 는 최단거리.
-while True:
-    for i in load[X - 1]:
-        pass
-print(load)
+
+distance = [0 for _ in range(N + 1)]  # 거리 정보 기록용
+visited = [0 for _ in range(N + 1)]  # 방문 체크용
+queue = deque([X])
+
+visited[X] = 1  # 시작위치 추가
+
+while queue:
+    now = queue.popleft()  # 현재 위치
+    for j in load[now]:
+        if visited[j] == 0:  # 방문하지 않은 도시라면(최단 거리이므로 해당 조건으로 제외시킴)
+            queue.append(j)
+            visited[j] = 1
+            distance[j] = distance[now] + 1
+
+# 조건에 맞는 도시 출력
+result = [i for i in range(1, N + 1) if distance[i] == K]
+print("\n".join(map(str, result)) if result else -1)
